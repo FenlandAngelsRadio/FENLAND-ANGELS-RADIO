44ef6b1bebf183179f839adc120fbbb238791552
FenlandAngelsRadio <fenlandangelsradio@gmail.com>
2026-09-03 02:37:11 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE='fenland-angels-radio-compact-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./fenland-angels-radio-logo.jpg','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.hostname.includes('yesstreaming.net')||e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match('./index.html'))));
-});
