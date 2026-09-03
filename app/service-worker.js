47cf51480a5661b04c72ea6662f9485bd57b790f
FenlandAngelsRadio <fenlandangelsradio@gmail.com>
2026-09-03 02:55:34 +0100

    Add files via upload

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='fenland-angels-phone-app-v3';
+const CACHE='fenland-angels-phone-app-v4';
 const ASSETS=['./','./index.html','./style.css?v=3','./app.js?v=3','./manifest.webmanifest','./fenland-angels-radio-logo.jpg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
